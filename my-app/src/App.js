2bcfc716ec64e732cfd9defb4e6990c20899d302
Michael <mioncgp@gmail.com>
2020-10-04 14:08:34 +0400

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,89 @@
 import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation.js";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import Logo from "./components/Logo/Logo";
+
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: "",
+      imageUrl: "",
+      box: {},
+      route: "signin",
+      isSignedIn: false,
+      user: {
+        id: "",
+        name: "",
+        email: "",
+        entries: 0,
+        joined: "",
+      },
+    };
+  }
+
+  onInputChange = (e) => {
+    this.setState({ input: e.target.value });
+  };
+
+  onButtonSubmit = (e) => {
+    console.log("lol");
+  };
+
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.date,
+      },
+    });
+  };
+
   render() {
-    return <div className="App"></div>;
+    const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+      <div className="App">
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+          </div>
+        ) : route === "signin" ? (
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
+      </div>
+    );
   }
 }
 
--- node/server.js
+++ node/server.js
@@ -3,10 +3,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const knex = require("knex");
 const bcrypt = require("bcrypt-nodejs");
-const register = require("./controllers/register");
-const signin = require("./controllers/signin");
-const profile = require("./controllers/profile");
-const image = require("./controllers/image");
+// const register = require("./controllers/register");
+// const signin = require("./controllers/signin");
+// const profile = require("./controllers/profile");
+// const image = require("./controllers/image");
 
 const db = knex({
   client: "pg",
@@ -24,25 +24,70 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json(database);
+  res.send("lol");
 });
 
 app.post("/signin", (req, res) => {
-  signin.handleSignin(req, res, db, bcrypt);
-});
-
-app.post("/register", (req, res) => {
-  register.handleRegister(req, res, db, bcrypt);
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("incorrect form submission");
+  }
+  db.select("*")
+    .from("login")
+    .where("email", "=", email)
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("something went wrong");
+      }
+    })
+    .catch((err) => res.status(400).json("unable to register"));
 });
 
 app.get("/profile/:id", (req, res) => {
-  profile.handleProfile(req, res, db);
+  const { id } = req.params;
+  db("users")
+    .select("*")
+    .where("id", "=", id)
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("something went wrong");
+      }
+    })
+    .catch((err) => res.status(400).json("unable to register"));
 });
 
-app.put("/image", (req, res) => {
-  image.handleImage(req, res, db);
+app.put("/posts", (req, res) => {
+  const { post_id } = req.body;
+  db("posts")
+    .select("*")
+    .where("post_id", "=", post_id)
+    .then((posts) => {
+      if (posts.length) {
+        res.json(posts[0]);
+      } else {
+        res.status(400).json("something went wrong");
+      }
+    })
+    .catch((err) => res.status(400).json("unable to register"));
 });
 
-app.listen(3000, () => {
-  console.log("app is runnung on port 3000");
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  db("users")
+    .returning("*")
+    .insert({
+      name: name,
+      email: email,
+      joined: new Date(),
+    })
+    .then((user) => res.json(user[0]))
+    .catch((err) => res.status(400).json("unable to register"));
+});
+
+app.listen(3001, () => {
+  console.log(`app is runnung on port ${3001}`);
 });
